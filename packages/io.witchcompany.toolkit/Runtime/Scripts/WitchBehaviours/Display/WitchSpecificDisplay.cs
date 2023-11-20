e24d56430ff8d07c0d36a32181cd79bab3c7bde6
HuiJiChoi <hjchoi@witchcompany.io>
2023-11-20 12:29:10 +0900

    WitchSpecific Display NonObj 받도록 수정

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchSpecificDisplay.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/Display/WitchSpecificDisplay.cs
@@ -10,23 +10,19 @@ namespace WitchCompany.Toolkit.Module
         public override string Description => "지정 에셋을 등록하여 전시할 수 있는 액자입니다.";
         public override string DocumentURL => "";
         public override int MaximumCount => 20;
-        // [field: Header("구매 유도 오브젝트")]
-        // [field: SerializeField] public GameObject NonObject { get; private set; }
+        [field: Header("구매 유도 오브젝트")]
+        [field: SerializeField] public GameObject NonObject { get; private set; }
         
         [field: Header("구매 페이지 Url")]
         [field: SerializeField] public String TargetUrl { get; private set; }
-        
-        // [field: Header("전시 이벤트")]
-        // [field: SerializeField] public UnityEvent OnDisplayEvent { get; private set; }
-        // [field: SerializeField] public UnityEvent OnHideDisplayEvent { get; private set; }
         
 #if UNITY_EDITOR
         public override ValidationError ValidationCheck()
         {
             if (String.IsNullOrWhiteSpace(TargetUrl)) return Error("targetUrl이 비어있습니다.");
-            // if (NonObject == null) return NullError(nameof(NonObject));
-            // if (!NonObject.TryGetComponent(out Collider _))
-            //     return new ValidationError("NonObject에 Collider가 있어야 합니다", context: NonObject);
+            if (NonObject == null) return NullError(nameof(NonObject));
+            if (!NonObject.TryGetComponent(out Collider _))
+                return new ValidationError("NonObject에 Collider가 있어야 합니다", context: NonObject);
             
             return null;
         }
