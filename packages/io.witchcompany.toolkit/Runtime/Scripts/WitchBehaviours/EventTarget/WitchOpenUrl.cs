8e80599dc2b0eca1a2f194321902bef830ec38a2
witch-kang <jhkang@witchcompany.io>
2023-12-05 17:30:08 +0900

    WitchOpenUrl : 개발/운영 서버에 따른 targetUrl 지정

--- packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/EventTarget/WitchOpenUrl.cs
+++ packages/io.witchcompany.toolkit/Runtime/Scripts/WitchBehaviours/EventTarget/WitchOpenUrl.cs
@@ -10,17 +10,22 @@ namespace WitchCompany.Toolkit.Module
         public override string Description => "지정한 url을 새 탭에서 열 수 있는 요소입니다.";
         public override string DocumentURL => "https://www.notion.so/witchcompany/WitchOpenUrl-50adfa83eea141e08c58953d09c6f73b?pvs=4";
 
-        [Header("새 탭에서 열 URL")]
+        [Header("운영 Url")]
         [SerializeField, TextArea] private string targetUrl;
+        [Header("개발 Url")]
+        [SerializeField, TextArea] private string targetUrlDev;
 
         public UnityAction<string> OpenUrlAction;
         public void OpenUrl() => OpenUrlAction?.Invoke(targetUrl);
         
+        public string TargetUrl => targetUrl;
+        public string TargetUrlDev => targetUrlDev;
+        
 #if UNITY_EDITOR
         public override ValidationError ValidationCheck()
         {
-            if (string.IsNullOrEmpty(targetUrl))
-                return Error("targetUrl이 비었습니다.");
+            if (string.IsNullOrEmpty(targetUrl)) return NullError(nameof(targetUrl));
+            if (string.IsNullOrEmpty(targetUrlDev)) return NullError(nameof(targetUrlDev));
 
             return base.ValidationCheck();
         }
